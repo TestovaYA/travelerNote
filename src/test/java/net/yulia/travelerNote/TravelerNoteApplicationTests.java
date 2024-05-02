0ea21e185157fd4eb9e8f6290c76eceaa303432c
Yuliya Testova <yuliya.testova@ural.rt.ru>
2024-05-02 16:34:06 +0500

    Initial commit

---
+++ src/test/java/net/yulia/travelerNote/TravelerNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yulia.travelerNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
