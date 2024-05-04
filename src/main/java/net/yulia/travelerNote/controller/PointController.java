18313d3485edcfeadc472f16bc1a2e6bb1901d9d
Yuliya Testova <yuliya.testova@ural.rt.ru>
2024-05-04 23:54:37 +0500

    fix /api/point/create (POST)

--- src/main/java/net/yulia/travelerNote/controller/PointController.java
+++ src/main/java/net/yulia/travelerNote/controller/PointController.java
@@ -5,18 +5,15 @@ import net.yulia.travelerNote.dto.PointDto;
 import net.yulia.travelerNote.service.PointService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/point/create")
+@RequestMapping("/api/point")
 @AllArgsConstructor
 public class PointController {
     PointService pointService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<PointDto> createPoint(@RequestBody PointDto pointDto){
         PointDto savedPoint = pointService.createPoint(pointDto);
         return new ResponseEntity<>(savedPoint, HttpStatus.CREATED);
