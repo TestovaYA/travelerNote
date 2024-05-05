3fcd942db6761f1bb3ecd4a2a8bb3dec46067734
Yuliya Testova <yuliya.testova@ural.rt.ru>
2024-05-05 19:41:36 +0500

    fix

--- src/main/java/net/yulia/travelerNote/controller/PointController.java
+++ src/main/java/net/yulia/travelerNote/controller/PointController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/point")
+@RequestMapping("/api/points")
 @AllArgsConstructor
 public class PointController {
     PointService pointService;
